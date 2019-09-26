0f487b20034423bc8a84fbb2c366850788fcd534
Ubuntu <ubuntu@ip-172-31-25-71.ec2.internal>
2019-09-26 18:13:25 +0000

    This program adds two numbers

---
+++ add.py
@@ -0,0 +1,15 @@
+# !user/bin/env python3
+
+# Created by: RJ Fromm
+# Created on: September 2019
+# This is an addition program
+
+first_number = int(input("Please enter first number : "))
+
+second_number = int(input("Please enter second number : "))
+
+sum_of_first_number_second_number = first_number + second_number
+
+print("The sum of {} + {} is : {}".
+format(first_number,second_number,sum_of_first_number_second_number))
+
